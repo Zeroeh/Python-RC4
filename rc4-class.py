d1fb1ea22d3106691360b37907625aeb622030bf
Zeroeh Dev <Zeroeh@users.noreply.github.com>
2017-12-19 15:39:22 -0600

    Update and rename onefun.py to rc4-class.py
    Removed garbage, cleaning stuff up

--- rc4-class.py
+++ rc4-class.py
@@ -6,53 +6,9 @@ decrypt_key = '72c5583cafb6818995cdd74b80'
 
 failure_packet = b's \xb4;\xc6\x90\x0f\xd4\xf4k\xe1s\xc8\x81\xa53\xe8\xbei-\xed\xa2IZCd\x00\x00\x00\x12\x00P\xcb\x84\x9b\x11,(\xf4:\xa9>\x0f\xbc\xff'
 
-# notr working because state of p and q are not saved upon using byteGenerator()
-
-#state = [None] * 256
-#p = q = None
-'''
-def setKey(key):
-	global p, q, state
-	state = [n for n in range(256)]
-	p = q = j = 0
-	for i in range(256):
-		j = (j + state[i] + key[i % len(key)]) % 256
-		state[i], state[j] = state[j], state[i]
-
-def byteGenerator():
-	global p, q, state
-	p = (p + 1) % 256
-	q = (q + state[p]) % 256
-	state[p], state[q] = state[q], state[p]
-	return state[(state[p] + state[q]) % 256]
-
-def encrypt(key, plaintext):
-	pt = string_to_list(plaintext)
-	ct = rc4(key, pt)
-	return list_to_string(ct)
-
-def decrypt(key, ciphertext):
-	ct = string_to_list(ciphertext)
-	pt = rc4(key, ct)
-	return list_to_string(pt)
-
-def rc4(key, ints):
-	setKey(string_to_list(key))
-	return [x ^ byteGenerator() for x in ints]
-'''
-
-
-
 class BotCrypto(object):
 	decrypt_key = '72c5583cafb6818995cdd74b80' # key to decrypt incoming packets
 	encrypt_key = '311f80691451c71d09a13a2a6e' # key to encrypt outgoing packets
-	rsa_pub_key = ("-----BEGIN PUBLIC KEY-----\n"
-					"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDCKFctVrhfF3m2Kes0FBL/JFeO\n"
-					"cmNg9eJz8k/hQy1kadD+XFUpluRqa//Uxp2s9W2qE0EoUCu59ugcf/p7lGuL99Uo\n"
-					"SGmQEynkBvZct+/M40L0E0rZ4BVgzLOJmIbXMp0J4PnPcb6VLZvxazGcmSfjauC7\n"
-					"F3yWYqUbZd/HCBtawwIDAQAB\n"
-			   		"-----END PUBLIC KEY-----")
-
 	p = None
 	q = None
 
@@ -98,14 +54,6 @@ class BotCrypto(object):
 	def guidEncrypt(string):
 		pass
 
-#endregion
-
-'''
-data = byte string
-key = string 
-encrypt = boolean
-'''
-
 def main():
 	print(failure_packet) #we receive raw hex from server
 	yaz = bytes.hex(failure_packet) #we change the raw bytes to something comprehensible
@@ -118,8 +66,6 @@ def main():
 	print(gaz)
 	waz = bytes.fromhex(gaz) #convert it back to binary puke that the server understands
 	print(waz)
-	#doo = BotCrypto.rc4(yaz, decrypt_key, False).encode('utf-8')
-	#print(doo)
 
 if __name__ == '__main__':
 	main()
